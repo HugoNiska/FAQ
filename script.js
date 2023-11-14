de48cb89b2e01a5a2ca6278c9bf77209329fc4f2
Springmask666 <145430236+Springmask666@users.noreply.github.com>
2023-11-14 15:02:54 +0100

    klar

--- script.js
+++ script.js
@@ -17,18 +17,17 @@ section3Element.addEventListener("click", toggle);
 function createFAQ(data) {
   const accordion = document.querySelector(".accordion");
   for (const post of data) {
-    // Create title element
     const titleElement = document.createElement("div");
     titleElement.className = "title section";
     titleElement.textContent = post.title;
-    // Create description element
+
     const descriptionElement = document.createElement("div");
     descriptionElement.className = "description";
     descriptionElement.innerHTML = `<p>${post.body}</p>`;
-    // Append title and description to the accordion
+
     accordion.appendChild(titleElement);
     accordion.appendChild(descriptionElement);
-    // Add click event listener to the title
+
     titleElement.addEventListener("click", () => {
       titleElement.classList.toggle("active");
       descriptionElement.style.display =
@@ -36,8 +35,6 @@ function createFAQ(data) {
     });
   }
 }
-// Fetch data from the provided URL
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then((response) => response.json())
-  .then((data) => createFAQ(data))
-  .catch((error) => console.error("Error fetching data:", error));
+  .then(createFAQ);
